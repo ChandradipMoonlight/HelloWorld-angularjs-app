da48780feabca3bc42b688950eb8b46a74d44e47
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-08-21 19:43:54 +0530

    Validated: userInput

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,28 @@ export class AppComponent {
   imgUrl: string;
   url: string ;
   userName: string = "";
+  nameError: string;
 
 
 ngOnInit(): void {
   this.title = "Hello form BridgeLabz.";
-  this.imgUrl = "../assets/Bridgelabz.jpg";
+  this.imgUrl = "../assets/BridgeLabz-2.jpg";
   this.url = "https://www.bridgelabz.com/";
+  this.userName = "";
 }
 
 onClick($event: Object) {
   console.log("save button is cliked!", $event);
   window.open(this.url, "_blank")
+}
+
+onInput($event: any) {
+  console.log("Change event Occured#", $event.data);
+  const nameRegex = RegExp ('^[A-Z][a-zA-Z\\s]{2,}$');
+  if (nameRegex.test(this.userName)) {
+    return this.nameError = "";
+  } else {
+    return this.nameError = "Name is Incorrect!";
+  }
 }
 }
